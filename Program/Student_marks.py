3cc92fe5928d649f39966cc29058edffbe40ea4c
Mohit Nath <mohitgoswami8870@gmail.com>
2025-07-11 20:39:13 +0530

    new commit

---
+++ Program/Student_marks.py
@@ -0,0 +1,19 @@
+marks = int(input("Enter your marks: "))
+print(f"Ypur marks are", marks)
+
+if (marks >= 90):
+    print("you got 'O' grade")
+elif(marks >= 80):
+    print("you got 'A' grade")
+elif(marks >= 70):
+    print("you got 'B' grade")
+elif(marks >= 60):
+    print("you got 'C' grade")
+elif(marks >= 50):
+    print("you got 'D' grade")  
+elif(marks >= 40):
+    print("you got 'E' grade")
+else:
+    print("you got 'F' grade : best of luck next time")
+ 
+
