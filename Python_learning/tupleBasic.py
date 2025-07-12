c95469119a149de9cfcbbe9762cda4156a8f215b
Mohit Nath <mohitgoswami8870@gmail.com>
2025-07-12 17:30:17 +0530

    inut movies

---
+++ Python_learning/tupleBasic.py
@@ -0,0 +1,9 @@
+x = str(input("Enter 1 Fovirate movie: "))
+y = str(input("Enter 2 Fovirate movie: "))
+z = str(input("Enter 3 Fovirate movie: "))
+
+list =[]
+list.append(x)
+list.append(y)  
+list.append(z)
+print("list:", list)
