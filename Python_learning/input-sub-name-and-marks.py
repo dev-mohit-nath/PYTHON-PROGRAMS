6fa00b11d1e4b9c0fb6ff7c0b0b1c8cd42c1c036
Mohit Nath <mohitgoswami8870@gmail.com>
2025-07-13 11:49:46 +0530

    set

---
+++ Python_learning/input-sub-name-and-marks.py
@@ -0,0 +1,9 @@
+marks = {}
+x = int(input("Enter phy: "))
+marks.update({"phy" : x})
+x = int(input("Enter math: "))
+marks.update({"math" : x})
+x = int(input("Enter hindi: "))
+marks.update({"hindi" : x})
+
+print(marks)
